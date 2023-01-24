66741fe359d052de2f24d1eb6564d17c3e0be399
Gizem <gizemustahuseyin123@gmail.com>
2023-01-24 21:27:57 +0300

    Add files via upload

---
+++ MutlakDeger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._14._1_IfElseYapisi
+{
+    class MutlakDeger
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n < 0)
+            {
+                Console.WriteLine($"|{n}| = {n * -1}");
+            }
+            else
+            {
+                Console.WriteLine($"|{n}| = {n}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
